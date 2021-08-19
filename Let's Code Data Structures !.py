ee79fd75e8e8a3c512e50f63da7b59407183113e
WarGusX <80565153+WarGusX@users.noreply.github.com>
2021-08-19 19:02:03 +0530

    Add files via upload

---
+++ Let's Code Data Structures !.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Write a Python program which accepts the radius of a circle from the user and computes area
+
+# In[ ]:
+
+
+from math import pi
+radius = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(radius) + " is: " + str(pi * radius**2))
+
