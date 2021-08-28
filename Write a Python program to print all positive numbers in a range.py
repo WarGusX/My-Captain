abc0e6b604780afdca1b0b0d6e10664a38e4aa89
WarGusX <80565153+WarGusX@users.noreply.github.com>
2021-08-28 19:24:18 +0530

    Add files via upload

---
+++ Write a Python program to print all positive numbers in a range.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Write a Python program to print all positive numbers in a range
+
+
+# In[10]:
+
+
+list1 = [12, -7, 5, 64, -14]
+num = 0
+
+while(num < len(list1)):
+    if list1[num] >= 0:
+        print(list1[num], end = " ")
+    num += 1
+    
+
+
+# In[11]:
+
+
+list2 = [12, 14, -95, 3]
+num = 0
+
+while(num < len(list2)):
+   if list2[num] >= 0:
+       print(list2[num], end = " ")
+   num += 1
+   
+
